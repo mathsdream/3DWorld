3e2a294e6877bdab942a49e0d3b1157b6b77210c
mathdream <2088126722@qq.com>
2024-08-09 00:15:36 +0800

    WIP

--- 3DWorld/3DWorldTest.cpp
+++ 3DWorld/3DWorldTest.cpp
@@ -9,15 +9,16 @@ int main()
     Point3D point2(1, 2, 3);
     Line3D line1(point1, point2);
     std::cout << "The length of line1 is " << line1.GetLength() << std::endl;
-
-    Model3D model;
     Point3D point3(1, 2, 3);
-    if (model.AddPoint(point1))
-        std::cout << "Add point1 successfully." << std::endl;
-    if (model.AddPoint(point2))
-        std::cout << "Add point2 successfully." << std::endl;
-    if (model.AddPoint(point3))
-        std::cout << "Add point3 successfully." << std::endl;
+
+    // 如果允许用户直接调用line的成员变量
+    // line1.points_[0] = point3;
+    // 这样就会导致line1的两个端点重合
+
+    // 所以我们不允许用户直接调用line的成员变量
+    if (!line1.SetPoint(point3, 0))
+        std::cerr << "Failed to set the point." << std::endl;
+    std::cout << "The length of line1 is " << line1.GetLength() << std::endl;
 
     return 0;
 }
